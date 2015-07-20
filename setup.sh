1d46fafe153bae526808f19f799f96d711cf32f2
Jace Ju <jaceju@gmail.com>
2015-07-20 16:05:41 +0800

    Fix problems of setup script

--- setup.sh
+++ setup.sh
@@ -15,9 +15,17 @@ else
   echo '----------'
 
   # @see https://about.gitlab.com/downloads/
-  sudo apt-get -y -qq install curl openssh-server ca-certificates postfix
+  sudo debconf-set-selections <<< "postfix postfix/mailname string phpci.local"
+  sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Local only'"
+  sudo apt-get -y -qq install curl openssh-server ca-certificates postfix gdebi
   curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
-  sudo apt-get -y -qq install gitlab-ce
+
+  if [ ! -e /vagrant/gitlab-ce_7.12.2~omnibus.1-1_amd64.deb ]; then
+    cd /tmp && wget https://packages.gitlab.com/gitlab/gitlab-ce/packages/ubuntu/trusty/gitlab-ce_7.12.2~omnibus.1-1_amd64.deb/download \
+            && mv /tmp/gitlab-ce_7.12.2~omnibus.1-1_amd64.deb /vagrant
+  fi;
+
+  sudo gdebi -n /vagrant/gitlab-ce_7.12.2~omnibus.1-1_amd64.deb
   sudo mkdir -p /etc/gitlab
   sudo cp /vagrant/gitlab.rb /etc/gitlab
   sudo gitlab-ctl reconfigure
